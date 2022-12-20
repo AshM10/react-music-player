da50dd5cf5163193abfb27613ef001c5c283c724
Ash Moreno <ashterfuentes@gmail.com>
2022-12-20 10:11:04 -0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Player from "./component/Player";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Player />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Player.jsx
@@ -0,0 +1,61 @@
+import { useEffect, useState } from "react";
+import useSound from "use-sound"; // for handling the sound
+import qala from "../assets/qala.mp3"; // importing the music
+import { AiFillPlayCircle, AiFillPauseCircle } from "react-icons/ai"; // icons for play and pause
+import { BiSkipNext, BiSkipPrevious } from "react-icons/bi"; // icons for next and previous track
+import { IconContext } from "react-icons"; // for customazing the icons
+import "../App.css";
+
+// conditional rendering the play/pause icon
+const [isPlaying, setIsPlaying] = useState(false);
+// useSound will return the play, pause, duration and sound method
+const [play, { pause, duration, sound }] = useSound(qala);
+
+// handling the play and pause buttons
+const playingButton = () => {
+  if (isPlaying) {
+    pause(); // this will pause the audio
+    setIsPlaying(false);
+  } else {
+    play(); // this will play the audio
+    setIsPlaying(true);
+  }
+};
+
+// export default function Player() {
+//   return (
+//     <div className="component">
+//       <h2>Playing Now</h2>
+//       <img className="musicCover" src="https://picsum.photos/200/200" />
+//       <div>
+//         <h3 className="title">Rubaiyyan</h3>
+//         <p className="subTitle">Qala</p>
+//       </div>
+//       <div>
+//         <button className="playButton">
+//           <IconContext.Provider value={{ size: "3em", color: "#27AE60" }}>
+//             <BiSkipPrevious />
+//           </IconContext.Provider>
+//         </button>
+//         {!isPlaying ? (
+//           <button className="playButton" onClick={playingButton}>
+//             <IconContext.Provider value={{ size: "3em", color: "#27AE60" }}>
+//               <AiFillPlayCircle />
+//             </IconContext.Provider>
+//           </button>
+//         ) : (
+//           <button className="playButton" onClick={playingButton}>
+//             <IconContext.Provider value={{ size: "3em", color: "#27AE60" }}>
+//               <AiFillPauseCircle />
+//             </IconContext.Provider>
+//           </button>
+//         )}
+//         <button className="playButton">
+//           <IconContext.Provider value={{ size: "3em", color: "#27AE60" }}>
+//             <BiSkipNext />
+//           </IconContext.Provider>
+//         </button>
+//       </div>
+//     </div>
+//   );
+// }
